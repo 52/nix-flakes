f386b9b7dfc5c4a84363a3516144753130b168b3
Max Karou <maxkarou@protonmail.com>
2025-07-14 18:42:06 +0200

    feat: added `node` development flake

---
+++ node/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Development environment flake for node";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        inherit (nixpkgs) lib;
+
+        # packages
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = lib.concatLists [
+            (with pkgs; [
+              nodejs_22
+              typescript
+              typescript-language-server
+            ])
+          ];
+          shellHook = ''
+            echo ""
+            echo "Using node versions:"
+            echo "$(${pkgs.nodejs_22}/bin/node --version)"
+            echo "$(${pkgs.nodejs_22}/bin/npm --version)"
+            echo ""
+          '';
+        };
+      }
+    );
+}
