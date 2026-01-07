d44a63d30ca4114f968bef8dd16002634228a876
Max Karou <maxkarou@protonmail.com>
2026-01-07 10:40:26 +0100

    feat: use ninos-unstable in `agent` env

--- env/agent/flake.nix
+++ env/agent/flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment flake for AI agents";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
