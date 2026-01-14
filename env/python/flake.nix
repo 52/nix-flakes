f6c640a43a9752a21971b202f255e8944e939103
Max Karou <maxkarou@protonmail.com>
2026-01-14 22:41:52 +0100

    feat: add python environment

---
+++ env/python/flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Development environment flake for python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        inherit (nixpkgs) lib;
+
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        ext_lib = import ../../lib {
+          inherit lib pkgs;
+        };
+
+        ## Available Python versions.
+        ##
+        #@ [String]
+        defaultVersions = [
+          "311"
+          "312"
+          "313"
+        ];
+
+        ## Additional packages to install.
+        ##
+        #@ [Package]
+        extraPackages = with pkgs; [
+          ruff
+          ty
+          uv
+        ];
+
+        ## Python package for each version.
+        ##
+        #@ AttrSet
+        runtimes = lib.genAttrs defaultVersions (version: pkgs."python${version}");
+      in
+      {
+        devShells = ext_lib.mkDevShells {
+          default = "312";
+          variants = runtimes;
+          packages = extraPackages;
+          shellHook = version: ''
+            export NIX_FLAKE_NAME="python:${version}"
+            export UV_PYTHON="${runtimes.${version}}/bin/python"
+            export UV_PYTHON_DOWNLOADS="never"
+
+            echo "python:  $(python --version)"
+            echo "uv:      $(uv --version)"
+            echo "ruff:    $(ruff --version)"
+            echo "ty:     $(ty --version)"
+          '';
+        };
+      }
+    );
+}
